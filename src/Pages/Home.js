c441bea54a04f18e527328a29f3bc5c22be4eea7
Siva prakash <30351639+Sivaprakash22@users.noreply.github.com>
2020-06-17 18:06:12 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,79 +1,87 @@
-import React, { Component } from 'react';
-import { Col, Button, FormGroup, Input, Card, CardBody } from 'reactstrap';
-import '../stlye.css'
-import db from '../services/firebase'
+import React, { Component } from "react";
+import { Col, Button, FormGroup, Input, Card, CardBody } from "reactstrap";
+import "../stlye.css";
+import db from "../services/firebase";
 
 class Home extends Component {
-
-
-
   getData() {
+    const name = document.getElementById("name");
+    const wish = document.getElementById("wish");
+    const msg = document.getElementById("msg");
 
-    const name = document.getElementById('name');
-    const wish = document.getElementById('wish');
-    const msg = document.getElementById('msg');
+    if (name.value === "" || wish.value === " ") {
+      alert("kindly Say you Name or wish");
+      return;
+    }
 
-    db.collection('wishes').add({
-      Name : name.value,
-      Wish : wish.value,
-      Message : msg.value
-    }).then(function (docRef) {
-      console.log("Document written with ID: ", docRef.id);
-    })
+    db.collection("wishes-ilk")
+      .add({
+        Name: name.value,
+        Wish: wish.value,
+        Message: msg.value,
+      })
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      });
 
     name.value = "";
     wish.value = "";
     msg.value = "";
-    
-    alert("Thank You!😊")
 
+    alert("Thank You!");
   }
-
 
   render() {
     return (
       <React.Fragment>
-
         <div className="box">
           <Card>
             <CardBody>
               <h2>Give your wishes</h2>
               <FormGroup row>
                 <Col sm={10}>
-                  <Input type="text" name="name" id="name" placeholder="Enter your Name" />
+                  <Input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Enter your Name"
+                  />
                 </Col>
               </FormGroup>
 
               <FormGroup row>
                 <Col sm={10}>
-                  <Input type="text" name="wish" id="wish" placeholder="Wish (eg : Happy Birthday!) " />
+                  <Input
+                    type="text"
+                    name="wish"
+                    id="wish"
+                    placeholder="Wish (eg : Happy Birthday!) "
+                  />
                 </Col>
               </FormGroup>
-
 
               <FormGroup row>
                 <Col sm={10}>
-                  <Input type="textarea" name="text" id="msg" placeholder="words to sudha" />
+                  <Input
+                    type="textarea"
+                    name="text"
+                    id="msg"
+                    placeholder="words to ilakkiya"
+                  />
                 </Col>
               </FormGroup>
-
 
               <FormGroup>
                 <Col>
                   <Button onClick={this.getData}>Submit</Button>
                 </Col>
               </FormGroup>
-
             </CardBody>
           </Card>
         </div>
-
-
-      </React.Fragment>)
+      </React.Fragment>
+    );
   }
 }
 
 export default Home;
-
-
-
